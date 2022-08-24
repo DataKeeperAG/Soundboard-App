e0cce5d8d4bc5d184c83d457206b082b3f0f4900
DataKeeperAG <124931553+DataKeeperAG@users.noreply.github.com>
2022-08-24 11:57:03 -0400

    20220823: fixed preferences bug, added splash screen

--- app/src/main/java/activities/SplashActivity.java
+++ app/src/main/java/activities/SplashActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 //import androidx.preference.PreferenceManager;
 
 import com.example.soundboard.R;
+
+import lib.Utils;
 //import com.example.thirteen_stones.thirteen_stones.lib.DialogUtils;
 
 
@@ -14,16 +17,15 @@ public class SplashActivity extends AppCompatActivity {
 
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
-//        DialogUtils.setNightModeOnOffFromPreferenceValue(getApplicationContext(), getString(R.string.night_mode_key));
-//
-//        startActivity (new Intent(getApplicationContext (),
-//                MainActivity.class));
-//        finish ();
-//
-//    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
+        Utils.setNightModeOnOffFromPreferenceValue(getApplicationContext(), getString(R.string.dark_mode_key));
+
+        startActivity (new Intent(getApplicationContext (), MainActivity.class));
+        finish ();
+
+    }
 
 }
